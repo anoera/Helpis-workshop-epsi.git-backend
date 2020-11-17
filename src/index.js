9f90582e5ba77e2707959fa5edd10a5b9a68d744
Arnaud Gonin <arnaud.gonin@live.fr>
2020-11-17 06:19:22 +0100

    Add api connected to Watson IA

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-const coucou = 'coucou';
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(coucou);
+const { watsonServices } = require('./services/watson.services');
+
+const app = express();
+const port = 4020;
+
+app.use(bodyParser.json());
+
+app.post('/tone-analyzer', async (req, res) => {
+	const data = req.body.data;
+
+	const result = await watsonServices.analyse(data);
+
+	switch (result.status) {
+		case 200:
+			res.status(200).send(result);
+			break;
+
+		default:
+			res.status(500).send();
+			break;
+	}
+});
+
+app.listen(port, () => {
+	console.log(`server running on http://127.0.0.1:${port}`);
+});
+
+exports.app = app;
---
+++ src/services/watson.services.js
@@ -0,0 +1,39 @@
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const { WATSON_VERSION, WATSON_APIKEY, WATSON_URL } = process.env;
+
+const toneAnalyzer = initToneAnalyzer();
+
+async function analyse(data) {
+	const toneParams = {
+		toneInput: { text: data },
+		contentType: 'application/json',
+	};
+
+	try {
+		return await toneAnalyzer.tone(toneParams);
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+}
+
+exports.watsonServices = {
+	analyse,
+};
+
+// Private Function
+
+function initToneAnalyzer() {
+	return new ToneAnalyzerV3({
+		version: WATSON_VERSION,
+		authenticator: new IamAuthenticator({
+			apikey: WATSON_APIKEY,
+		}),
+		serviceUrl: WATSON_URL,
+		headers: {
+			'X-Watson-Learning-Opt-Out': 'true',
+		},
+	});
+}
